689949f8a717710578e670c6c92c0c395307e42c
Bruno Quindeler <bruno.quindeler@gmail.com>
2022-06-01 05:31:35 -0300

    feat: add const to host

--- main.go
+++ main.go
@@ -5,11 +5,13 @@ import (
 	"net/http"
 )
 
+const HOST = ":8080"
+
 func main() {
 	http.HandleFunc("/view/", makeHandler(viewHandler))
 	http.HandleFunc("/edit/", makeHandler(editHandler))
 	http.HandleFunc("/save/", makeHandler(saveHandler))
 
-	log.Println("Server started on: localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Printf("server started on: %s", HOST)
+	log.Fatal(http.ListenAndServe(HOST, nil))
 }
